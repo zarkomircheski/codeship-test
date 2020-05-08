22a0b03871c79a5fbb0db8d3d5eb5e3ff32bc8ff
CI_COMMITTER_NAME <CI_COMMITTER_EMAIL>
2020-05-08 22:29:00 +0000

    Deployment to codeship-test by codeship_ci from

--- deploy_script.sh
+++ deploy_script.sh
@@ -8,12 +8,12 @@ USERNAME=zarkomircheski
 TOKEN=e37452bad8c6149033514d87a2923e314e19a836
 REPO=codeship-test
 
-git remote add [REPO] https://[USERNAME]:[TOKEN]@github.com/[USERNAME]/[REPO].git
+git remote add ${REPO} https://${USERNAME}:${TOKEN}@github.com/${USERNAME}/${REPO}.git
 
 git config --global user.email CI_COMMITTER_EMAIL
 git config --global user.name CI_COMMITTER_NAME
 git config core.ignorecase false
 git add --all
-git commit -am "Deployment to ${target_wpe_install} $REPO by $CI_COMMITTER_NAME from $CI_NAME"
+git commit -am "Deployment to $REPO by $CI_COMMITTER_NAME from $CI_NAME"
 
-git push [REPO] master
+git push ${REPO} master
