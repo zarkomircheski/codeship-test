630afe771682b8842dd6be84e865353a166e73eb
CI_COMMITTER_NAME <CI_COMMITTER_EMAIL>
2020-05-13 20:05:32 +0000

    Deployment to codeship-test by codeship_ci from

--- build_script.sh
+++ build_script.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 # If any commands fail (exit code other than 0) entire script exits
 
-echo "---> Installing npm packages"
-npm install
-gulp --version
+ls -la
 
-# Install composer packages
-composer install --prefer-dist --no-scripts --no-autoloader && rm -rf /root/.composer
-composer dump-autoload --no-scripts --optimize
+ln -s node_packages/node_modules app/node_modules
+ln -s composer_packages/vendor app/vendor
+
+cd composer_packages
+ls -la
+
+cd ../node_packages
+ls -la
+
+cd ../app
+ls -la
+
+# echo "---> Installing npm packages"
+# npm install
+# gulp --version
+
+# # Install composer packages
+# composer install --prefer-dist --no-scripts --no-autoloader && rm -rf /root/.composer
+# composer dump-autoload --no-scripts --optimize
--- wp-content/themes/grandfather/functions/gulp-scripts.php
+++ wp-content/themes/grandfather/functions/gulp-scripts.php
@@ -2,6 +2,6 @@
 
 define('MAIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '');
 define('MAIN_SCRIPT', fth_get_protocol_relative_template_directory_uri() . '');
-define('ADMIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/admin-pSDozLylgta9hG8YJWeD8SONlVmtMdIz.css');
-define('AMP_STYLE', '/dist/css/amp-17kXqdspxD5x51ghaML4oBLDIgdFqPm0.css');
+define('ADMIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/admin-NzVwwDXWDTafl7H3sYtrig8oysh6h3h5.css');
+define('AMP_STYLE', '/dist/css/amp-3kPSzI71IKKg4CCCVx8VMtdwMhWBgbTA.css');
 define('FAW_STYLE', fth_get_protocol_relative_template_directory_uri() . '');
