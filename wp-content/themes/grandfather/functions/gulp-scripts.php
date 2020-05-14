4f8f75264902889641c6f2934bcc8cf97d52fc80
CI_COMMITTER_NAME <CI_COMMITTER_EMAIL>
2020-05-14 18:42:36 +0000

    Deployment to codeship-test by codeship_ci from

--- wp-content/themes/grandfather/functions/gulp-scripts.php
+++ wp-content/themes/grandfather/functions/gulp-scripts.php
@@ -1,7 +1,7 @@
 <?php
 
-define('MAIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/app-qA0TvZOxD2OExIMXsCmKhCukPcdILbPB.css');
-define('MAIN_SCRIPT', fth_get_protocol_relative_template_directory_uri() . '/dist/js/app-NpE9K3rGU8242YCvFS2aXPnEFpksk3dV.js');
-define('ADMIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/admin-o0lAa2DKzHI4m7blHtFCggrtmnnql4NH.css');
-define('AMP_STYLE', '/dist/css/amp-GXArvtsgEPfhRywU0bkTYzIliVo5dmda.css');
-define('FAW_STYLE', '/dist/css/faw-txXVEWhzgs1v3bLxLmK3ajkaFO6jJpDQ.css');
+define('MAIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/app-AuoEnsZ4Lpogt6Ausz5GoOgTdJyUsMcd.css');
+define('MAIN_SCRIPT', fth_get_protocol_relative_template_directory_uri() . '/dist/js/app-1PI3lxcfh0VnojDFPUzFUPJzqzFoIlUC.js');
+define('ADMIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/admin-Dx2eiunQeDPPcuiO0j7Yl3zZU3F8pYFJ.css');
+define('AMP_STYLE', '/dist/css/amp-eHk0igLpat0EOhwmdZOKEmwI8QRJJspp.css');
+define('FAW_STYLE', '/dist/css/faw-VxBuYPosgFulontWcXmzO1j70U3j9vfw.css');
