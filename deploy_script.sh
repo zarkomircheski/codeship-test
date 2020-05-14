c425ef3142d11766a5a3f1b3e0c1347b726f79e3
CI_COMMITTER_NAME <CI_COMMITTER_EMAIL>
2020-05-14 19:02:18 +0000

    Deployment to codeship-test by codeship_ci from

--- deploy_script.sh
+++ deploy_script.sh
@@ -11,13 +11,16 @@ USERNAME=zarkomircheski
 TOKEN=e37452bad8c6149033514d87a2923e314e19a836
 REPO=codeship-test
 
-git remote add ${REPO} https://${TOKEN}@github.com/${USERNAME}/${REPO}.git
+# git remote add ${REPO} https://${TOKEN}@github.com/${USERNAME}/${REPO}.git
 
-git config --global user.email CI_COMMITTER_EMAIL
-git config --global user.name CI_COMMITTER_NAME
+git config user.email CI_COMMITTER_EMAIL
+git config user.name CI_COMMITTER_NAME
 git config core.ignorecase false
+
 git add --all
-git commit -am "Deployment to $REPO by $CI_COMMITTER_NAME from $CI_NAME"
+
+git status
+git commit -m "Deployment to $REPO by $CI_COMMITTER_NAME from $CI_NAME"
 
 git push ${REPO} master
 
--- wp-content/themes/grandfather/functions/gulp-scripts.php
+++ wp-content/themes/grandfather/functions/gulp-scripts.php
@@ -1,7 +1,7 @@
 <?php
 
-define('MAIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/app-AuoEnsZ4Lpogt6Ausz5GoOgTdJyUsMcd.css');
-define('MAIN_SCRIPT', fth_get_protocol_relative_template_directory_uri() . '/dist/js/app-1PI3lxcfh0VnojDFPUzFUPJzqzFoIlUC.js');
-define('ADMIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/admin-Dx2eiunQeDPPcuiO0j7Yl3zZU3F8pYFJ.css');
-define('AMP_STYLE', '/dist/css/amp-eHk0igLpat0EOhwmdZOKEmwI8QRJJspp.css');
-define('FAW_STYLE', '/dist/css/faw-VxBuYPosgFulontWcXmzO1j70U3j9vfw.css');
+define('MAIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/app-kTHWd0atJSfTI8ve5Tm4wmGyV9GvtY4t.css');
+define('MAIN_SCRIPT', fth_get_protocol_relative_template_directory_uri() . '/dist/js/app-vgyURnQcbZrFZBJZkhS7Vy95kreejWXH.js');
+define('ADMIN_STYLE', fth_get_protocol_relative_template_directory_uri() . '/dist/css/admin-fSVUsWRZZczVGtRhprmrUIA5yY8Mmkd4.css');
+define('AMP_STYLE', '/dist/css/amp-wz9a7H9CESeITaCBBgdKO4HrkGBei3Jm.css');
+define('FAW_STYLE', '/dist/css/faw-Naf5Q7e31i55QEbrKgARFpAM4xYApcGu.css');
